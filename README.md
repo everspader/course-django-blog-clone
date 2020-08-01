342323d985a6af87647267ee683c5007aaef3b5d
Everton Spader <everton.spader@gmail.com>
2020-08-01 10:35:44 +0200

    Uploading relevant files of project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blog Clone Project
+
+This repo contains the Blog Clone project done during the Python and Django fullstack course.
---
+++ blogclone/blog/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    # Get_absolut_url works to get the url to where the page should
+    # be redirected once a new post is published. In this case
+    # it will go back to the PostDetailView referenced as 'post_detail'
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments',
+                            on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    # Same happens here. When a comment is created and approved by
+    # a super user, it takes the user back to the list of posts in
+    # PostListView referenced here as 'post_list'
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
